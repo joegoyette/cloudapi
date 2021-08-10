c5606e2ddd744fe342933f3da5c515eed3b064ef
joegoyette <joe.goyette@gmail.com>
2021-08-09 23:26:22 -0500

    update

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -5,7 +5,7 @@ namespace App\Console;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
-use app\Jobs\UpdatePrices;
+use App\Jobs\UpdatePrices;
 
 class Kernel extends ConsoleKernel
 {
