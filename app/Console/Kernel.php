2d4b633adfbf3d023fd74433f2729e805f4481da
joegoyette <joe.goyette@gmail.com>
2021-08-09 23:23:17 -0500

    update

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -27,7 +27,7 @@ class Kernel extends ConsoleKernel
     protected function schedule(Schedule $schedule)
     {
         // $schedule->command('inspire')->hourly();
-        $schedule->job(new UpdatePrices)->everyHour();
+        $schedule->job(new UpdatePrices)->hourly();
     }
 
     /**
